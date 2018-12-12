7fbf4f73210e1993cd996c5d72dc6c545a3d86a5
Nick Justin <Nicholasgjustin@gmail.com>
2018-12-12 14:25:08 -0600

    Created new store function called findAndUpdate. It takes an id and newData obj. Finds store item with id and returns a new object with the store item and newData obj merged.

--- scripts/index.js
+++ scripts/index.js
@@ -2,11 +2,22 @@
 /* global shoppingList, global api, cuid */
 
 // eslint-disable-next-line no-unused-vars
-const store = {
-  items: [],
-  hideCheckedItems: false,
-  searchTerm: ''
-};
+const store = (function(){
+  const items = [];
+  let hideCheckedItems = false;
+  let searchTerm = '';
+  function findAndUpdate(id, newData){
+    return Object.assign(this.items.find(function(element){
+      return element.id === id;
+    }), newData);
+  }
+  return {
+    items,
+    hideCheckedItems,
+    searchTerm,
+    findAndUpdate
+  };
+}());
 
 $(document).ready(function() {
   api.getItems((items) => {
