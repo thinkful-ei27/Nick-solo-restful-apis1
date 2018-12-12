0cff70c37107fa90bf4384abb94b8e28f551d700
Nick Justin <Nicholasgjustin@gmail.com>
2018-12-12 11:14:40 -0600

    created createItem function in api.js. Tested with thinkful server for a POST request. Creates new item on the server

--- scripts/api.js
+++ scripts/api.js
@@ -7,10 +7,22 @@ const api =(function(){
   function getItems(callback){
     $.getJSON(`${BASE_URL}/items`, callback);
   }
+  
+  function createItem(name, callback){
+    const newItem = JSON.stringify({name: name});
+    $.ajax({
+      url: `${BASE_URL}/items`,
+      method: 'POST',
+      contentType: 'application/json',
+      data: newItem,
+      success: callback
+    });
+  }
 
   return {
     BASE_URL,
-    getItems
+    getItems,
+    createItem
   };
 }());
 
